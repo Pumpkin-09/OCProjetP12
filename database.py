f2cf82979215a2787fe598fbbeca8c1e687da404
pumpkin <badoalan@gmail.com>
2025-09-25 16:45:43 +0200

    first commit

---
+++ database.py
@@ -0,0 +1,26 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+
+# Charger les variables d'environnement depuis le fichier .env
+load_dotenv()
+
+# Charger les informations de connexion depuis les variables d'environnement
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "3306")  # Valeur par défaut si non spécifiée
+DB_USER = os.getenv("DB_USERNAME", "user")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
+DB_NAME = os.getenv("DB_DATABASE", "db_name")
+
+# Construire l'URL de connexion à la base de données
+SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Créer l'engine SQLAlchemy
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+
+# Configurer la session
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Déclarer une base pour les modèles ORM
+Base = declarative_base()
---
+++ models/models.py
@@ -0,0 +1,61 @@
+from database import Base, engine
+from sqlalchemy import Column, Integer, String, Date, DateTime, Float, Boolean, ForeignKey, Enum
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+import enum
+
+
+class MyEnum(enum.Enum):
+    gestion = "equipe gestion"
+    commertial = "equipe commercial"
+    support = "equipe support"
+
+
+class Client(Base):
+    __tablename__ = "client"
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    nom_complet = Column(String(150), nullable=False)
+    email = Column(String(250), nullable=False, unique=True)
+    telephone = Column(String(14), nullable=False)
+    nom_entreprise = Column(String(250), nullable=False)
+    date_creation = Column(Date, default=func.curdate())
+    date_mise_a_jour = Column(DateTime, default=func.current_timestamp(), onupdate=func.current_timestamp())
+    id_collaborateur = Column(Integer, ForeignKey("collaborateur.id"), nullable=False)
+
+
+class Collaborateur(Base):
+    __tablename__ = "collaborateur"
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    nom = Column(String(150), nullable=False)
+    email = Column(String(250), nullable=False, unique=True)
+    password = Column(String(250), nullable=False)
+    role = Column(Enum(MyEnum), nullable=False)
+
+
+class Contrat(Base):
+    __tablename__ = "contrat"
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id_client = Column(Integer, ForeignKey("client.id"), nullable=False)
+    id_collaborateur = Column(Integer, ForeignKey("collaborateur.id"), nullable=False)
+    montant_total = Column(Float, nullable=False)
+    reste_a_payer = Column(Float, nullable=False, nullable=True)
+    date_creation_contrat = Column(Date, default=func.curdate())
+    status_contrat = Column(Boolean, default=False)
+
+
+class Evenement(Base):
+    __tablename__ = "evenement"
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name_event = Column(String(150), nullable=False)
+    id_client = Column(Integer, ForeignKey("client.id"), nullable=False)
+    id_collaborateur = Column(Integer, ForeignKey("collaborateur.id"), nullable=False)
+    event_date_start = Column(Date, nullable=False)
+    event_date_stop = Column(Date, nullable=False)
+    id_support_contrat = Column(Integer, ForeignKey("collaborateur.id"), nullable=False)
+    location = Column(String(300), nullable=True)
+    attente = Column(Integer, nullable=True)
+    note = Column(String(500), nullable=True)
